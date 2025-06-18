2a0e6e062830226022c58163ea3a7464e339f7cf
reyhanderyam <128274946+reyhanderyam@users.noreply.github.com>
2025-06-18 10:43:02 -0500

    Update and.v

--- src/and.v
+++ src/and.v
@@ -1,1 +1,16 @@
+/*
+A simple 8-bit AND in Verilog, TOP. 
+*/
 
+module tt_um_and(
+  input clk,
+  input reset,
+  input [7:0] a, b,
+  output [7:0] Y
+);
+
+  assign Y = a & b;
+
+       
+
+endmodule
